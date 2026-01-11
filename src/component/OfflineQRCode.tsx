1706064d5349b600190e4135400b7187a9f99f67
Shrinx21 <fayyadhaz21@gmail.com>
2026-01-11 09:13:41 +0700

    Update OfflineQRCode.tsx

--- src/component/OfflineQRCode.tsx
+++ src/component/OfflineQRCode.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+
 import React, { useEffect, useState } from "react";
 import QRCodeLib from "qrcode";
 import { Product } from "../lib/data";
+import { QR_DOMAIN } from "../lib/qrconfig";
 
 type Props = { product: Product };
 
@@ -12,14 +14,8 @@ const OfflineQRCode: React.FC<Props> = ({ product }) => {
   useEffect(() => {
     let isMounted = true;
 
-    const textPayload = `nama: ${product.nama}\nnim: ${product.nim}\nno_hp: ${product.no_hp}\nnama_bisnis: ${product.nama_bisnis}\ntanggal_berdiri: ${product.tanggal_berdiri}\nkategori_bisnis: ${product.kategori_bisnis}\nnama_produk: ${product.nama_produk}\nid: ${product.id}\nnomer_induk_barang: ${product.nomer_induk_barang}\nlokasi_barang: ${product.lokasi_barang}\nstok_barang: ${product.stok_barang}\nharga_produk: ${product.harga_produk}\ntanggal_diserahkan: ${product.tanggal_diserahkan}\nfoto_produk: ${product.foto_produk}`;
-
-    // Prefer PDF link when id is available (adjust as needed)
-    const preferredUrl = (typeof window !== "undefined" && product.id)
-      ? `${window.location.origin}/pdfs/${encodeURIComponent(product.id)}.pdf`
-      : null;
-
-    const qrValue = preferredUrl ?? textPayload;
+    // Always generate QR code for /print/${nama_bisnis} using QR_DOMAIN
+    const qrValue = `${QR_DOMAIN}/print/${encodeURIComponent(product.nama_bisnis)}`;
 
     QRCodeLib.toDataURL(qrValue, { margin: 1, scale: 6 })
       .then((url) => {
